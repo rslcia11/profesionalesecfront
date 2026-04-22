030f2393331ca4af35d464886260231478468fad
JuanseDev2002 <188579428+juanse-campana@users.noreply.github.com>
2026-04-21 23:16:29 -0500

    m

--- profesionalesecfront/app/page.tsx
+++ profesionalesecfront/app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
       <Header />
       <Hero />
       <Services />
+      <FeaturedProfessionals />
       <Convenios />
       <ConnectingProfessionals />
-      <FeaturedProfessionals />
       <Footer />
     </main>
   )
